ddf988dc9d269b969ac5b525ceb8e7483191eff0
Thiago Ueda <thiagokoiti@gmail.com>
2024-09-03 14:44:02 -0300

    adiciona Tarefa.java e ListaTarefa.java em OperacoesBasicas/exercicios

--- src/main/java/list/OperacoesBasicas/exercicios/ListaTarefa.java
+++ src/main/java/list/OperacoesBasicas/exercicios/ListaTarefa.java
@@ -1,4 +1,43 @@
 package main.java.list.OperacoesBasicas.exercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaTarefa {
+    // Atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        if (!tarefaList.isEmpty()) {
+            for (Tarefa tarefa : tarefaList) {
+                if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefasParaRemover.add(tarefa);
+                }
+            }
+            tarefaList.removeAll(tarefasParaRemover);
+        } else {
+            System.out.println("A lista esta vazia!");
+        }
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        if (!tarefaList.isEmpty()) {
+            System.out.println(tarefaList);
+        } else {
+            System.out.println("A lista esta vazia!");
+        }
+    }
 }
--- src/main/java/list/OperacoesBasicas/exercicios/Tarefa.java
+++ src/main/java/list/OperacoesBasicas/exercicios/Tarefa.java
@@ -1,4 +1,14 @@
 package main.java.list.OperacoesBasicas.exercicios;
 
 public class Tarefa {
+    // Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
