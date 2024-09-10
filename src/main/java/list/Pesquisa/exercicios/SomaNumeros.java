5b9bf81412fad3121137663caa77b92a112c8539
Thiago Ueda <thiagokoiti@gmail.com>
2024-09-10 17:20:16 -0300

    Adiciona as classes do exercicios CarrinhoDeCompras

---
+++ src/main/java/list/Pesquisa/exercicios/SomaNumeros.java
@@ -0,0 +1,75 @@
+package main.java.list.Pesquisa.exercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+    // Atributos
+    private List<Integer> numeros;
+
+    public SomaNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for(Integer n: numeros) {
+            soma += n;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = Integer.MIN_VALUE;
+        if(!numeros.isEmpty()) {
+            for(Integer numero: numeros) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        if(!numeros.isEmpty()) {
+            for (Integer numero: numeros) {
+                if (numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+            return menorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    public void exibirNumeros() {
+        if(!numeros.isEmpty()) {
+            System.out.println(this.numeros);
+        } else {
+            System.out.println("A lista está vazia");
+        }
+    }
+
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+
+        System.out.println(somaNumeros.calcularSoma());
+        System.out.println(somaNumeros.encontrarMaiorNumero());
+        System.out.println(somaNumeros.encontrarMenorNumero());
+        somaNumeros.exibirNumeros();
+    }
+}
